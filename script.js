045df47d288c23f8985b4ad0393de6e0497382ab
ausperg <web@apm-it.eu>
2026-04-28 18:51:01 +0200

    improved the recipe

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Profi Pizza 24h 20cm
 const rFlour = 100;
-const rSalt = 2.4;
-const rWaterBeginner = 64;
+const rSalt = 3;
+const rWaterBeginner = 65;
 const rWaterPro = 70;
-const rOil = 2.5;
-const rYeast8h = 1.0;
-const rYeast16h = 0.8;
-const rYeast24h = 0.6;
+const rOil = 3;
+const rYeast3h = 1.0;
+const rYeast24h = 0.2;
+const rYeast48h = 0.1;
 const rPizzaSize = 20; // 20cm
 
 const btnDarkMode = document.getElementById("darkmode-btn");
@@ -24,9 +24,9 @@ const dryYeast = document.getElementById("dry-yeast");
 
 const btnBeginner = document.getElementById("beginner-level");
 const btnPro = document.getElementById("pro-level");
-const btn8H = document.getElementById("eight-hours");
-const btn16H = document.getElementById("sixteen-hours");
+const btn3H = document.getElementById("three-hours");
 const btn24H = document.getElementById("twenty-four-hours");
+const btn48H = document.getElementById("forty-eight-hours");
 
 var isPro = true;
 var doughTime = 24;
@@ -99,18 +99,10 @@ btnPro.addEventListener("click", function () {
 	setIngredients();
 });
 
-btn8H.addEventListener("click", function () {
-	doughTime = 8;
+btn3H.addEventListener("click", function () {
+	doughTime = 3;
 	this.style.background = 'var(--primary-weak)';
-	btn16H.style.background = 'var(--bg)';
-	btn24H.style.background = 'var(--bg)';
-	setIngredients();
-});
-
-btn16H.addEventListener("click", function () {
-	doughTime = 16;
-	this.style.background = 'var(--primary-weak)';
-	btn8H.style.background = 'var(--bg)';
+	btn48H.style.background = 'var(--bg)';
 	btn24H.style.background = 'var(--bg)';
 	setIngredients();
 });
@@ -118,8 +110,16 @@ btn16H.addEventListener("click", function () {
 btn24H.addEventListener("click", function () {
 	doughTime = 24;
 	this.style.background = 'var(--primary-weak)';
-	btn8H.style.background = 'var(--bg)';
-	btn16H.style.background = 'var(--bg)';
+	btn3H.style.background = 'var(--bg)';
+	btn48H.style.background = 'var(--bg)';
+	setIngredients();
+});
+
+btn48H.addEventListener("click", function () {
+	doughTime = 48;
+	this.style.background = 'var(--primary-weak)';
+	btn3H.style.background = 'var(--bg)';
+	btn24H.style.background = 'var(--bg)';
 	setIngredients();
 });
 
@@ -136,17 +136,17 @@ function setIngredients() {
 		water.innerHTML = (rWaterBeginner * factor).toFixed(0);
 	oil.innerHTML = (rOil * factor).toFixed(0);
 	switch (doughTime) {
-		case 8:
-			yeast.innerHTML = (rYeast8h * factor).toFixed(1);
-			dryYeast.innerHTML = (rYeast8h * factor / 3).toFixed(1);
-			break;
-		case 16:
-			yeast.innerHTML = (rYeast16h * factor).toFixed(1);
-			dryYeast.innerHTML = (rYeast16h * factor / 3).toFixed(1);
+		case 3:
+			yeast.innerHTML = (rYeast3h * factor).toFixed(1);
+			dryYeast.innerHTML = (rYeast3h * factor / 3).toFixed(1);
 			break;
 		case 24:
 			yeast.innerHTML = (rYeast24h * factor).toFixed(1);
 			dryYeast.innerHTML = (rYeast24h * factor / 3).toFixed(1);
+			break;
+		case 48:
+			yeast.innerHTML = (rYeast48h * factor).toFixed(1);
+			dryYeast.innerHTML = (rYeast48h * factor / 3).toFixed(1);
 			break;
 		default:
 			yeast.innerHTML = (rYeast24h * factor).toFixed(1);
